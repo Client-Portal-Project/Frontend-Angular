5d3abb9660379dc2f0e0ab13d8166fe7fb2d2517
Rearles <53582049+Rearles@users.noreply.github.com>
2021-12-16 14:34:05 -0700

    Copy over what I did on the other staging project Angular Front End

--- src/app/components/resetemail/resetemail.component.ts
+++ src/app/components/resetemail/resetemail.component.ts
@@ -1,17 +1,24 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-resetemail',
   templateUrl: './resetemail.component.html',
   styleUrls: ['./resetemail.component.css']
 })
-  /*
-    As a user I want to be able to reset my email if I forget it.
-    This'll be implemented fully in a later iteration of this project where we would email the person at their email to reset their email to a new email.  
-  */
+/*
+  As a user I want to be able to reset my email if I forget it.
+  This'll be implemented fully in a later iteration of this project where we would email the person at their email to reset their email to a new email.  
+*/
 export class ResetemailComponent implements OnInit {
   @Input() userData = { Firstname: '', Lastname: '', Email: '', Password: '' };
-  constructor() { }
+  _isEmpty: boolean = false;
+  _emailisEmpty: boolean = false;
+  _passwordisEmpty: boolean = false;
+  _firstisEmpty: boolean = false;
+  _lastisEmpty: boolean = false;
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -22,7 +29,25 @@ export class ResetemailComponent implements OnInit {
   resetemail(): void {
     //implemenation notes:
     //Needs a way to make sure that the person is who they say that they are so that anyone can't just reset anyone else's account
-    //Given an email, send an email to reset the email and password already in the DB. 
-    console.log(this.userData)
+    //Given an email, send an email to reset the email and password already in the DB.
+    if (this.userData.Firstname == "") {
+      this._firstisEmpty = true;
+    }
+    else if (this.userData.Lastname == "") {
+      this._lastisEmpty = true;
+    }
+    else if (this.userData.Password == "") {
+      this._passwordisEmpty = true;
+    }
+    else if (this.userData.Email == "") {
+      this._emailisEmpty = true;
+    }
+    else {
+      this._isEmpty = false;
+      this.userService.editUser(this.userData).subscribe((response: { Firstname: string; Lastname: string; Email: string; Password: string; }) => {
+        this.userData = response;
+      })
+    }
+    alert('Your email has been reset to ' + this.userData.Email)
   }
 }
--- src/app/components/resetlogin/resetlogin.component.ts
+++ src/app/components/resetlogin/resetlogin.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserRegistrationService } from 'src/app/service/fetch-api-data.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-resetlogin',
@@ -18,12 +20,17 @@ The system will ask for the email associated with the account and send an email 
 
 */
 export class ResetloginComponent implements OnInit {
-
+  _isEmpty: boolean = false;
+  _emailisEmpty: boolean = false;
+  _passwordisEmpty: boolean = false;
+  _firstisEmpty: boolean = false;
+  _lastisEmpty: boolean = false;
+  user: any = {};
   @Input() userData = { Firstname: '', Lastname: '', Email: '', Password: '' };
-  constructor() {
+  constructor(private userService: UserService, private router: Router) {
   }
-  
-  
+
+
   ngOnInit(): void {
   }
   //this is for resetting passwords, we can have a different component for resetting emails
@@ -31,11 +38,30 @@ export class ResetloginComponent implements OnInit {
   //Either the email exists already in the database, in which case, potential popup that says "Email sent to X to reset password"
   //Or the email doesn't exist, in which case, we just say "We are sorry, we can't find your email"
   resetlogin(): void {
+    //if email isn't empty
+    if (this.userData.Firstname == "") {
+      this._firstisEmpty = true;
+    }
+    else if (this.userData.Lastname == "") {
+      this._lastisEmpty = true;
+    }
+    else if (this.userData.Password == "") {
+      this._passwordisEmpty = true;
+    }
+    else if (this.userData.Email == "") {
+      this._emailisEmpty = true;
+    }
+    else {
+      this._isEmpty = false;
+      this.userService.editUser(this.userData).subscribe((response: { Firstname: string; Lastname: string; Email: string; Password: string; }) => {
+        this.userData = response;
+      })
+    }
     //implementation notes:
     //call backend to look up email sent through by form submittion
     //if it exists, great, send email
     //if it doesn't exist, tell user that it isn't a valid email in DB and that they need to enter another email
-    console.log(this.userData)
+    alert('Successfully reset password for the account ' + this.userData.Email);
   }
 
 
