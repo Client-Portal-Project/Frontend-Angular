bdadf9cf7bab7bb7d387c187b00515e1c0d93de2
Rearles <53582049+Rearles@users.noreply.github.com>
2021-12-21 14:25:17 -0700

    Start to code out the applicant document upload component

--- src/app/components/applicant-upload/applicant-upload.component.ts
+++ src/app/components/applicant-upload/applicant-upload.component.ts
@@ -10,20 +10,31 @@ import { FileService } from 'src/app/services/file.service';
   ///Fileuploadercomponent handles the showing of the place to upload as well as actually uploading the file.
   /*
   Given:
-  An Applicant has an account.
+  An Applicant has an account. this is done through making sure they are logged in
 
   When:
-  The Applicant uploads the documents in question to their page.
+  The Applicant uploads the documents in question to their page. Going to keep the fileuploader separate for potential use later but use the functionality there. 
 
   Then:
-  The System has those docs ready for the Owner to use when dealing with Clients
+  The System has those docs ready for the Owner to use when dealing with Clients Since the docs are in the applicant table in the db we can pull them from the database at a later date
 
+  Figure out how to set up the documents in question to show up "on their page" to view. 
   */
 export class ApplicantUploadComponent implements OnInit {
+  file!: File;
 
-  constructor(private fileupload: FileuploaderComponent) { }
-  ngOnInit(): void {
-    
+  filename!: string;
+  constructor(private fileupload: FileuploaderComponent) {
+
   }
-
+  ngOnInit(): void {
+    this.file = this.fileupload.file;
+    this.filename = this.fileupload.filename;
+  }
+  getFile(event: Event): void {
+    this.fileupload.getFile(event);
+  }
+  uploadFile() {
+    this.fileupload.uploadFile();
+  }
 }
