515b30e121247adeeb311ff875b74eb7e3dd8e32
Rearles <53582049+Rearles@users.noreply.github.com>
2022-01-06 11:18:42 -0700

    file service

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { ResponeMessage } from '../classes/ResponseMessage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileService {
 
-  constructor() { }
+  url = "http://localhost:3000/clientportal/api"
+
+  constructor(private client: HttpClient) { }
+
+  uploadFile(file: File): Observable<ResponeMessage> {
+    const formData = new FormData();
+    formData.append("file", file);
+    let message: string = "";
+    return this.client.post<ResponeMessage>(this.url + "/upload", formData);
+
+  }
+
+
+  getPDF() {
+    const httpOptions = {
+      'responseType': 'arraybuffer' as 'json'
+    };
+    return this.client.get<any>(this.url + "/files/4547d15e-6709-4d02-ab9a-2c4d2ae2f4ef", httpOptions)
+  }
+
+
 }
