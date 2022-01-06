e800de1861d589179fc253bd0f067101664bf00d
Rearles <53582049+Rearles@users.noreply.github.com>
2022-01-06 11:11:56 -0700

    fileversion component

--- src/app/components/fileversion/fileversion.component.ts
+++ src/app/components/fileversion/fileversion.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FileService } from 'src/app/services/file.service';
 
 @Component({
-  selector: 'app-fileversion',
-  templateUrl: './fileversion.component.html',
-  styleUrls: ['./fileversion.component.css']
+  selector: 'app-file-viewer',
+  templateUrl: './file-viewer.component.html',
+  styleUrls: ['./file-viewer.component.css']
 })
-export class FileversionComponent implements OnInit {
+export class FileViewerComponent implements OnInit {
 
-  constructor() { }
+  file!: File;
+
+  constructor(private fileService: FileService) { }
 
   ngOnInit(): void {
+
+    this.fileService.getPDF().subscribe(data => {
+      let file = new Blob([data], { type: "application/pdf" });
+      this.file = new File([data], "resume.pdf");
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(this.file);
+      link.download = this.file.name;
+      link.click();
+    })
+
+
+
+
   }
 
 }
