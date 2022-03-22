a665557b37e127f48bbc2d6096d31b5489cda25a
Brian Stiner <Brian.Stiner@revature.net>
2022-03-22 08:49:02 -0700

    re-organization

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -14,22 +14,19 @@ export class UserService {
   verifyUser(email: string | undefined): Observable<any> {
     return this.httpClient.get<any>(`${this.utilService.getServerDomain()}/clientportal/api/user/${email}`);
   }
-
   createUser(user: any): Observable<any> {
     return this.httpClient.post<any>(`${this.utilService.getServerDomain()}/clientportal/api/user`, user);
   }
-  
+  editUser(user: any): Observable<any> {
+    return this.httpClient.put<any>(`${this.utilService.getServerDomain()}/clientportal/api/user`, user, {'headers': this.utilService.headers});
+  }
+
   login(email: string, password: string): Observable<any> {
     return this.httpClient.post<any>(`${this.utilService.getServerDomain()}/clientportal/api/login`, {
       email: email,
       password: password
     }, {observe: "response"});
   }
-
-  editUser(user: any): Observable<any> {
-    return this.httpClient.put<any>(`${this.utilService.getServerDomain()}/clientportal/api/user`, user, {'headers': this.utilService.headers});
-  }
-
   logout() {
     sessionStorage.clear();
     this.router.navigateByUrl('');
