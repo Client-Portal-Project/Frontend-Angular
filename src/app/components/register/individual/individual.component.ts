ed01f92f56139c062f83ded2b8401f2feb25f9cd
Brian Stiner <Brian.Stiner@revature.net>
2022-03-28 11:34:05 -0700

    added form validation

--- src/app/components/register/individual/individual.component.ts
+++ src/app/components/register/individual/individual.component.ts
@@ -13,19 +13,17 @@ export class IndividualComponent implements OnInit{
   constructor(private service: ApplicantService) { }
 
   employmentStatuses = ['Employed','Unemployed','Self-Employed','Retired','Student','Other']
-  educationLevels = ['High School','Associate Degree','Bachelor Degree','Master Degree','PhD']
+  educationLevels = ['High School','Associate Degree','Bachelor Degree','Master Degree','PhD','Other']
   educationFields = ['Computer Science','Mathematics','Physics','Chemistry','Biology','Other']
   submitted = false;
-  model = new Applicant(
-    undefined, 'I am a cool person that likes to hang...',
+  model:Applicant = new Applicant(
+    undefined, 'I am...',
     this.educationLevels[0],this.educationFields[0],this.employmentStatuses[0]);
-    
+
   ngOnInit() {}
 
   onSubmit(): void {
     this.submitted = true;
+    console.log(this.model);
   }
-
-
-
 }
