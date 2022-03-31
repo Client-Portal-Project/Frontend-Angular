a6b26fa87794b0575dff7f180c5ae4d41ad79bb0
Brian Stiner <Brian.Stiner@revature.net>
2022-03-30 21:22:46 -0700

    mainpage with leftbar check our at /main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  // list of images, replace with any model with a model.image property
+  matches: any[] =
+  ["https://www.w3schools.com/howto/img_forest.jpg",
+  "https://www.w3schools.com/howto/img_snow.jpg",
+  "https://www.w3schools.com/howto/img_mountains.jpg",
+  "https://www.w3schools.com/howto/img_lights.jpg",
+  "https://www.w3schools.com/howto/img_mountains.jpg",
+  "https://www.w3schools.com/howto/img_forest.jpg"];
+
   constructor() { }
 
   ngOnInit(): void {
