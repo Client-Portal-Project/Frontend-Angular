a0e87f59f1d0ef3e6133482ffb817a1fd8f34b11
Hiroyuki0487 <Hiroyuki0487@users.noreply.github.com>
2022-06-20 15:17:57 -0400

    Profile Registration Progress

--- src/app/components/profile-form/profile-form.component.ts
+++ src/app/components/profile-form/profile-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { fromEventPattern } from 'rxjs';
 
 @Component({
   selector: 'app-profile-form',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileFormComponent implements OnInit {
 
+  registerForm = new FormGroup({
+    email:new FormControl(''),
+    password: new FormControl(''),
+    pwTest: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    
+  });
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  registerUser(registerForm: FormGroup){
+    if(this.registerForm.get('pass')?.value != this.registerForm.get('passTest')?.value || this.registerForm.get('pass')?.value == null)
+    this.router.navigate(['/register']);
+  else {
+  let sessUser: User = new User(this.registerForm.get('email')?.value, this.registerForm.get('pass')?.value, this.registerForm.get('firstName')?.value,
+  this.registerForm.get('lastName')?.value, this.registerForm.get('biography')?.value);
+  let userT = JSON.stringify(sessUser); 
+  this.userServ.registerUser(userT).subscribe(
+    response => {
+      console.log("Great success.");
+    },
+    error =>{
+      console.warn("An error has occurred");
+    }
+  )}
 }
+
+
+  }
+
+
